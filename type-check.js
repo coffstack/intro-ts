13fae518e73b4f021b240888ea016843306872bb
Lucas Garcez <lucasgarcezdev@gmail.com>
2025-01-02 16:59:13 +1100

    6 - dynamic vs static type system

---
+++ type-check.js
@@ -0,0 +1,9 @@
+// tipagem dinâmica
+// Dynamically Typed
+let message = "Hello World";
+
+message = 6;
+
+console.log(message.toUpperCase());
+
+// console.log(typeof message);
---
+++ type-check.ts
@@ -0,0 +1,7 @@
+// tipagem estática
+// Statically Typed language
+let message = "Hello World";
+
+// message = 6;
+
+console.log(message.toUpperCase());
